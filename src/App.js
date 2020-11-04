44ffcf307e779e71f295611772c89f16aa8aa7dd
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-11-04 15:53:04 +0000

    Add full-site authentication

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
 import Amplify, { Auth } from "aws-amplify";
 import awsconfig from "./aws-exports";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
 Amplify.configure(awsconfig);
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">App header content here</header>
+      <header className="App-header">
+        <AmplifySignOut />
+        <p>App header content here</p>
+      </header>
     </div>
   );
-}
+};
 
-export default App;
+export default withAuthenticator(App);
