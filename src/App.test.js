2e83c5580aa146f8a56f9c95ab6fc7011f938a58
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-11-05 12:18:49 +0000

    Modify tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,25 +1,30 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-import FilmCard from "./components/FilmCard";
+import { StyledFilmCard, StyledHeader } from "./components/styled/lib";
 
 /** @todo Mock authentication (first test currently renders amplify authenticator) */
-test("renders App without crashing", () => {
-  render(<App />);
+describe("App", () => {
+  test("renders App without crashing", () => {
+    render(<App />);
+  });
 });
 
-test("renders FilmCard without crashing", () => {
-  const film = { title: "test", year: 2000, likes: 2, description: "test" };
-  render(<FilmCard film={film} />);
-});
+describe("Components", () => {
+  test("StyledFilmCard renders and displays film title", () => {
+    const film = {
+      title: "testFilmTitle",
+      year: 2000,
+      likes: 2,
+      description: "test",
+    };
+    render(<StyledFilmCard film={film} />);
+    const titleElement = screen.getByText(/testFilmTitle/i);
+    expect(titleElement).toBeInTheDocument();
+  });
 
-test("FilmCard displays title", () => {
-  const film = {
-    title: "testTitle",
-    year: 2000,
-    likes: 2,
-    description: "testDescription",
-  };
-  render(<FilmCard film={film} />);
-  const titleElement = screen.getByText(/testTitle/i);
-  expect(titleElement).toBeInTheDocument();
+  test("StyledHeader renders and displays site title", () => {
+    render(<StyledHeader />);
+    const titleElement = screen.getByText(/Films Voter/i);
+    expect(titleElement).toBeInTheDocument();
+  });
 });
