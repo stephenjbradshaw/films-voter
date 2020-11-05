34891086efd965945eccd326576ba85c5de764d3
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-11-05 09:13:47 +0000

    Add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import FilmCard from "./components/FilmCard";
 
-test('renders learn react link', () => {
+// TODO: Mock authentication. First test currently renders amplify authenticator
+test("renders App without crashing", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders FilmCard without crashing", () => {
+  const film = { title: "test", year: 2000, likes: 2, description: "test" };
+  render(<FilmCard film={film} />);
+});
+
+test("FilmCard displays title", () => {
+  const film = {
+    title: "testTitle",
+    year: 2000,
+    likes: 2,
+    description: "testDescription",
+  };
+  render(<FilmCard film={film} />);
+  const titleElement = screen.getByText(/testTitle/i);
+  expect(titleElement).toBeInTheDocument();
 });
